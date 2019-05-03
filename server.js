df62654e0a8620c95e17cd90c5404cf8b248dfc4
Dennis <dennissarmiento808@gmail.com>
2019-05-03 15:15:57 -0700

    up

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
-const wineRoutes = require('./routes/api/Wine')
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/wines"
@@ -11,32 +11,33 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/',(req,res)=>(
-    db.MasterWineList.find({}).then(Wines=>{
-        res.json(Wines)
-    })
-))
+// app.get('/',(req,res)=>(
+//     db.MasterWineList.find({}).then(Wines=>{
+//         res.json(Wines)
+//     })
+// ))
 
-app.post('/',(req,res)=>{
-    console.log("req.body:", req.body)
-    return (
-    db.MasterWineList.create({
-        name: req.body.name,
-        primaryFlavors: req.body.primaryFlavors.split(','),
-        region: req.body.region
-    }).then(Wines=>
-        res.json(Wines)
-    )
-)})
+// app.post('/',(req,res)=>{
+//     console.log("req.body:", req.body)
+//     return (
+//     db.MasterWineList.create({
+//         name: req.body.name,
+//         primaryFlavors: req.body.primaryFlavors.split(','),
+//         region: req.body.region
+//     }).then(Wines=>
+//         res.json(Wines)
+//     )
+// )})
 
-app.delete('/:_id',(req,res)=>{
-    db.MasterWineList.findById(req.params._id)
-      .then(dbWine => dbWine.remove())
-      .then(dbWine => res.json(dbWine))
-      //if error, catch it and give a 422 response as well as logging the error in the console
-      .catch(err => res.status(422).json(err));
-})
+// app.delete('/:_id',(req,res)=>{
+//     db.MasterWineList.findById(req.params._id)
+//       .then(dbWine => dbWine.remove())
+//       .then(dbWine => res.json(dbWine))
+//       //if error, catch it and give a 422 response as well as logging the error in the console
+//       .catch(err => res.status(422).json(err));
+// })
 
-app.use('/api/wine', wineRoutes)
+app.use(routes)
+
 
 app.listen(PORT)
