6c0e575ff768878c76cd0366b66ac8c94a0d0e7c
Dennis <dennissarmiento808@gmail.com>
2019-05-05 22:24:54 -0700

    up

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,14 @@ const router = require("express").Router();
 const wineRoutes = require("./Wine");
 const restaurantRoutes = require("./Restaurants");
 const employeesRoutes = require("./Employees");
-
+const addwineRoutes = require("./Addwine");
 
 router.use("/employees", employeesRoutes);
 router.use("/restaurant", restaurantRoutes);
 router.use("/wine", wineRoutes);
+
+router.use("/addwine", addwineRoutes);
+
 
 
 
