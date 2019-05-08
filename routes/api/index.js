44b1d5ee9f642498841fa1964a2da7d82005537a
ruiligao <ruiligao@gmail.com>
2019-05-08 00:55:57 -0700

    rg

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,7 @@ const wineRoutes = require("./Wine");
 const restaurantRoutes = require("./Restaurants");
 const  employeesRoutes = require("./Employees");
 const signupLoginRoutes = require("./signupLogin");
-
+const addEmployeeRoutes = require("./AddEmployee");
 //naming the routes; using the books.js and google.js routes file
 
 //the API.js will hit these routes once activitated by client/ user
@@ -19,6 +19,7 @@ router.use("/restaurant", restaurantRoutes);
 router.use("/wine", wineRoutes);
 
 router.use("/addwine", addwineRoutes);
+router.use("/addEmployee", addEmployeeRoutes);
 
 router.use("/getwine", getwineRoutes);
 
