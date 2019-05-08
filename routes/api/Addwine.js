d08117a3db03f8a12dcf30842f28687feac0b675
Dennis <dennissarmiento808@gmail.com>
2019-05-08 12:02:05 -0700

    up

--- routes/api/Addwine.js
+++ routes/api/Addwine.js
@@ -3,6 +3,8 @@ const addwineController = require("../../controllers/addwineController");
 
 //defining our root route or "/"
 router.route("/")
+.post(addwineController.addWine)
+
 .put(addwineController.update)
 
 .delete(addwineController.remove);
