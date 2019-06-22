bdf356b0d71176bdacfd1a090242f9f3aea5090d
ruiligao <ruiligao@gmail.com>
2019-06-21 20:19:00 -0700

    added build folder

---
+++ build/precache-manifest.93e2bcca7b0f0393167571d044c9c3f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dafb2ad3982900ac4088",
+    "url": "/Tannin/static/js/runtime~main.b71e23b5.js"
+  },
+  {
+    "revision": "afc59267a59f2fb99818",
+    "url": "/Tannin/static/js/main.b5d96908.chunk.js"
+  },
+  {
+    "revision": "e5060feb8aba60263190",
+    "url": "/Tannin/static/js/2.081b986d.chunk.js"
+  },
+  {
+    "revision": "afc59267a59f2fb99818",
+    "url": "/Tannin/static/css/main.268b0c4d.chunk.css"
+  },
+  {
+    "revision": "721eea21b5884f8939602aeecf799018",
+    "url": "/Tannin/index.html"
+  }
+];
